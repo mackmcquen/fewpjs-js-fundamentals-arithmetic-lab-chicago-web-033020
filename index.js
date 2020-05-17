87b38f653da118e05ef10855887db076d7b5305e
mackmcquen <mackmcquen@comcast.net>
2020-05-17 17:00:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+var num1 = 1
+
+var num2 = 62
+
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random() > 0)
+
+var mod = 18 - 14
+
+var max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
